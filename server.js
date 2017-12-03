0d14f23fe680be5d0582cd544f767199ee495bad
Peter Huson <peter@huson.com>
2017-12-02 22:07:19 -0500

    Added some stuff

--- server.js
+++ server.js
@@ -4,14 +4,30 @@ var mysql = require('mysql')
 io = io.listen(3000, function(err){
     console.log("error thrown:" + err)
 })*/
-var server = require('http').createServer(function(req, res) {
+//console.log(__dirname)
+/*var server = require('http').createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(index.html);
+    res.end(res.sendFile(__dirname));
 });
 var io = require('socket.io')(server);
 
+server.listen(3000);*/
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', function(){ /* … */ });
 server.listen(3000);
 
+//var express = app();
+//app.use(express.static(__dirname, { index: 'index.html' }));
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+app.listen(process.env.PORT);
 // Define our db creds
 var db = mysql.createConnection({
     host: 'localhost',
@@ -48,6 +64,22 @@ io.sockets.on('connection', function(socket){
         io.sockets.emit('new note', data)
         // Use node's db injection format to filter incoming data
         db.query('INSERT INTO notes (note) VALUES (?)', data.note)
+    })
+
+    socket.on('max price', function(data){
+        // New note added, push to all sockets and insert into db
+        max = Math.round(data.amt)
+        var flights = []
+        // Filter all flights by the maximum price
+        db.query('SELECT * FROM flights WHERE price < (?)', max)
+            .on('result', function(flight){
+                // Push results onto the array of flights
+                flights.push(flight)
+            })
+            .on('end', function(){
+                // Only emit flights after query has been completed
+                socket.emit('we got da flights!', flights)
+            })
     })
  
     // Check to see if initial query/notes are set
